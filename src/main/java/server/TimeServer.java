bd7d8de62aaf60f8e749a1e7ad144d20c2818551
KnaldeKalle <cph-kj209@cphbusiness.dk>
2017-09-29 11:48:50 +0200

    New commit

--- src/main/java/server/TimeServer.java
+++ src/main/java/server/TimeServer.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
-/**
- *
- * @author KnaldeKalle
- */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Calendar;
+
+
 public class TimeServer {
+    
+    private static int PORT = 1234;
+    private static String IP = "localhost";  
+    private static ServerSocket serverSocket;
+    private static Calendar cal = Calendar.getInstance();
+    
+    public static void handleClient(Socket s) throws IOException{
+        
+        PrintWriter pw = new PrintWriter(s.getOutputStream(),true); // MORE IMPORTANT REMEMBER TRUE FOR AUTOFLUSH!!!!!!
+        //IMPORTANT BLOKING 
+        
+        pw.println("Recieved request for time...");
+        pw.println(cal.getTime());
+        System.out.println("Received request for time!");
+        System.out.println(cal.getTime());
+        s.close();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        if(args.length == 2){
+            PORT = Integer.parseInt(args[0]);
+            IP = args[1];
+        }
+        
+        serverSocket = new ServerSocket(); // Remember to bind
+        serverSocket.bind(new InetSocketAddress(IP,PORT));
+        System.out.println("Wating for a client!");
+        
+        while(true){
+        Socket socket = serverSocket.accept(); // IMPORTANT BLOCKING CALL
+        System.out.println("Client found!");
+        handleClient(socket);
+        }
+        
+        
+        
+    }
     
 }
